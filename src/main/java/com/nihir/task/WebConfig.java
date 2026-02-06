670b7959e604714e778716442b1ea686f5e79225
Nihir Verma <nihirverma14@gmail.com>
2026-02-06 15:25:54 +0530

    Fix CORS for Vercel

---
+++ src/main/java/com/nihir/task/WebConfig.java
@@ -0,0 +1,20 @@
+package com.nihir.task;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // This is the EXACT URL from your error message. Do not add a slash at the end.
+                .allowedOrigins("https://task-tracker-app-frontend-xtqy.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
